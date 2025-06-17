324804418bfd949c5652959afd78c0eae9908cbe
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-17 13:43:22 +0300

    permissions route

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use nextdev\nextdashboard\Http\Controllers\AdminController;
 use nextdev\nextdashboard\Http\Controllers\AuthController;
 use nextdev\nextdashboard\Http\Controllers\DropDownsController;
+use nextdev\nextdashboard\Http\Controllers\PermissionController;
 use nextdev\nextdashboard\Http\Controllers\TicketCategoriesController;
 use nextdev\nextdashboard\Http\Controllers\TicketController;
 
@@ -27,12 +28,15 @@ Route::prefix("dashboard")->group(function () {
         Route::prefix("tickets")->group(function () {
             // Tickets resource
             Route::apiResource('', TicketController::class)->parameters(['' => 'ticket']);
-            
+
             // Ticket Categories resource
             Route::apiResource('categories', TicketCategoriesController::class);
     
             Route::get('statuses', [DropDownsController::class, 'ticketStatuies']);
             Route::get('priorities', [DropDownsController::class, 'ticketPriorities']);
         });
+
+        // Roles & Permissions 
+        Route::GET("/permissions", [PermissionController::class, "index"]);
     });
 });
