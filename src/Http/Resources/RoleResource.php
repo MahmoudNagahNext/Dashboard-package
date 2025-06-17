7a0baab91850b1d6fcc75e0abd4461da993fdb3d
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-17 16:09:26 +0300

    implement Roles module

--- src/Http/Resources/RoleResource.php
+++ src/Http/Resources/RoleResource.php
@@ -14,7 +14,6 @@ class RoleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        dd($this->permissions);
         return [
             'id' => $this->id,
             'name' => $this->name,
