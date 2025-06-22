49f8cc42eedaf31a845d6a6486963ae0987e2ca0
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-22 16:39:34 +0300

    return drop down based on lang

--- src/Http/Controllers/DropDownsController.php
+++ src/Http/Controllers/DropDownsController.php
@@ -3,29 +3,35 @@
 namespace nextdev\nextdashboard\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use nextdev\nextdashboard\DTOs\AdminDTO;
 use nextdev\nextdashboard\Traits\ApiResponseTrait;
-use nextdev\nextdashboard\Http\Requests\Auth\LoginRequest;
-use nextdev\nextdashboard\Http\Requests\Auth\RegisterRequest;
-use nextdev\nextdashboard\Http\Resources\AdminResource;
 use nextdev\nextdashboard\Models\TicketPriority;
 use nextdev\nextdashboard\Models\TicketStatus;
-use nextdev\nextdashboard\Services\AuthService;
 
 class DropDownsController extends Controller
 {
     use ApiResponseTrait;
 
-    public function ticketStatuies()
+    public function ticketStatuses()
     {
-        $items = TicketStatus::all();
+        $items = TicketStatus::all()->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'name' => $item->getTranslation('name', app()->getLocale()),
+            ];
+        });
+
         return $this->successResponse($items);
     }
 
     public function ticketPriorities()
     {
-        $items = TicketPriority::all();
+        $items = TicketPriority::all()->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'name' => $item->getTranslation('name', app()->getLocale()),
+            ];
+        });
+
         return $this->successResponse($items);
     }
-
 }
