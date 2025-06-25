0dbc2b5da06b29956e27b71729e6b9020e064ee6
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-25 13:19:30 +0300

    ticket replay response

--- src/Http/Controllers/TicketReplyController.php
+++ src/Http/Controllers/TicketReplyController.php
@@ -5,17 +5,15 @@ namespace nextdev\nextdashboard\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
 use nextdev\nextdashboard\Events\TicketReplied;
 use nextdev\nextdashboard\Http\Requests\TicketReply\StoreTicketReplyRequest;
 use nextdev\nextdashboard\Http\Requests\TicketReply\UpdateTicketReplyRequest;
 use nextdev\nextdashboard\Http\Resources\TicketReplyResource;
 use nextdev\nextdashboard\Services\TicketReplyService;
-use nextdev\nextdashboard\Traits\ApiResponseTrait;
 
 class TicketReplyController extends Controller
 {
-    use ApiResponseTrait;
-
     public function __construct(
         protected TicketReplyService $ticketReplyService,
     ) {}
@@ -29,52 +27,50 @@ class TicketReplyController extends Controller
         $admin = $reply->admin;
 
         // Dispatch the event
-        event(new TicketReplied($ticket, $reply, $admin));
-
-        return $this->successResponse(
-            TicketReplyResource::make($reply),
-            "Ticket Reply created successfully",
-            201
-        );
+        // event(new TicketReplied($ticket, $reply, $admin));
+        return Response::json([
+            'success' => true,
+            'message' => 'Ticket Reply created successfully',
+            'data' => TicketReplyResource::make($reply)
+        ], 201);
     }
 
     public function index(Request $request, int $ticketId): JsonResponse
     {
         $replies = $this->ticketReplyService->index($ticketId);
-        return $this->successResponse(
-            TicketReplyResource::collection($replies),
-            "Ticket Replies retrieved successfully",
-            200
-        );
+        return Response::json([
+            'success' => true,
+            'message' => 'Ticket Replies retrieved successfully',
+            'data' => TicketReplyResource::collection($replies)
+        ], 200);
     }
 
     public function update(int $ticketId, int $id, UpdateTicketReplyRequest $request): JsonResponse
     {
         $reply = $this->ticketReplyService->update($id, $request->validated());
-        return $this->successResponse(
-            TicketReplyResource::make($reply),
-            "Ticket Reply updated successfully",
-            200
-        );
+        return Response::json([
+            'success' => true,
+            'message' => 'Ticket Reply updated successfully',
+            'data' => []
+        ], 200);
     }
 
     public function delete(int $ticketId, int $id): JsonResponse
     {
         $this->ticketReplyService->delete($id);
-        return $this->successResponse(
-            [],
-            "Ticket Reply deleted successfully",
-            200
-        );
+        return Response::json([
+            'success' => true,
+            'message' => 'Ticket Reply deleted successfully',
+            'data' => []
+        ], 200);
     }
 
     public function show(int $ticketId, int $id): JsonResponse
     {
-        $reply = $this->ticketReplyService->find($id);
-        return $this->successResponse(
-            TicketReplyResource::make($reply),
-            "Ticket Reply retrieved successfully",
-            200
-        );
+        return Response::json([
+            'success' => true,
+            'message' => 'Ticket Reply retrieved successfully',
+            'data' => TicketReplyResource::make($$this->ticketReplyService->find($id))
+        ], 200);
     }
 }
