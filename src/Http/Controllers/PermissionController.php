14752f80ec30912de04fcfc31e34c0404cb5c72f
MahmoudNagahNext <m.nagah@nextecontech.ai>
2025-06-25 12:53:30 +0300

    permission response

--- src/Http/Controllers/PermissionController.php
+++ src/Http/Controllers/PermissionController.php
@@ -1,24 +1,23 @@
 <?php 
 namespace nextdev\nextdashboard\Http\Controllers;
 
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
 use nextdev\nextdashboard\Services\PermissionService;
-use nextdev\nextdashboard\Traits\ApiResponseTrait;
 
 
 class PermissionController extends Controller
 {
-    use ApiResponseTrait;
-
     public function __construct(
         protected PermissionService $service
     ){}
     public function index()
     {
         $permissions = $this->service->groupedPermissions();
-        return $this->successResponse($permissions, "Permissions Pagination");
+        return Response::json([
+            'success' => true,
+            'message' => "Permissions Pagination",
+            'data' => $permissions
+        ]);
     }
 }
